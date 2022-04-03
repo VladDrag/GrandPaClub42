3012d02ec3d2f33289b731e4d92196ecaf877925
Vlad Dragomir <vdragomir@protonmail.com>
2022-04-03 17:33:12 +0200

    Added constructor for GrandPa class

--- Models/GrandPa.cs
+++ Models/GrandPa.cs
@@ -9,6 +9,16 @@
 		public string PreferredQuote { get; set; }
 		public string NickName { get; set; }
 
+		public GrandPa(int id, string firstName, string lastName, int age, string preferredQuote, string nickName)
+		{
+			Id = id;
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+			PreferredQuote = preferredQuote;
+			NickName = nickName;
+		}
+
 		public string DisplayName
 		{
 			get
