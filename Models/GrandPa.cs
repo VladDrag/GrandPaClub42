c975d6283dcffbe77d6b4e68813eeb0fb31f37ae
Vlad Dragomir <vdragomir@protonmail.com>
2022-04-03 17:18:30 +0200

    Added the GrandPa class and updated background colour for the site

---
+++ Models/GrandPa.cs
@@ -0,0 +1,28 @@
+﻿namespace GrandPaClub42.Models
+{
+	public class GrandPa
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public string PreferredQuote { get; set; }
+		public string NickName { get; set; }
+
+		public string DisplayName
+		{
+			get
+			{
+				return FirstName + " \"" + NickName + "\" " + LastName + ", " + Age;
+			}
+		}
+
+		public string DisplayInfo
+		{
+			get
+			{
+				return "Likes to say" + " \"" + PreferredQuote + "\" ";
+			}
+		}
+	}
+}
